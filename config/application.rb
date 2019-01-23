03f735f8a072e9d382bc692ae58092c08aa464bc
Alfred Dominguez <alfred.dominguez@gmail.com>
2019-01-23 18:44:25 +0000

    get app deploy ready

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# config/application.rb
-config.assets.initialize_on_precompile = false
-
-
-
 module AlphaBlog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
