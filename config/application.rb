4f558a4e82b465f1e5a7321572e54bae7c39cb15
Alfred Dominguez <alfred.dominguez@gmail.com>
2019-01-24 05:01:38 +0000

    get app production ready

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module AlphaBlog
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config/application.rb
-    
+    config.assets_compile = false
     
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
