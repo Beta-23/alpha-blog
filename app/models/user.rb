aa6961194807f520a1ea0f63513d074ea00134e2
Alfred Dominguez <alfred.dominguez@gmail.com>
2019-03-07 21:43:52 +0000

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
-  
-  
+  # vlaidates database username, checks for unique username and checks for case sensitive username and give min and max required
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum: 15 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+            length: { maximum: 50 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 end
