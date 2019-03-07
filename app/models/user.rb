39c8e7cda8afec374930c705b052accdfb3d5312
Alfred Dominguez <alfred.dominguez@gmail.com>
2019-03-07 21:57:50 +0000

    Modify user.rb file with comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  # vlaidates database username, checks for unique username and checks for case sensitive username and give min and max required
+# vlaidates database username, checks for unique username and checks for case sensitive username and gives min and max required
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
             length: { minimum: 3, maximum: 15 }
+  # vlaidates database email, checks for unique email, checks case sensitive email and max require.  
+  # REGEX- validates email address via regular expressions.
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
             length: { maximum: 50 },
