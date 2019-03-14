047891d58078476729ef5ee1d58bc10340a95732
Alfred Dominguez <alfred.dominguez@gmail.com>
2019-03-14 21:16:38 +0000

    Add user login sessions routes and controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  
+  def new
+  
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #session hash that will be remembered by the browser cookies
+      session[:user_id] = user.id
+      flash[:success] = "horay! you have logged in successfully"
+      redirect_to user_path(user)
+    
+    else
+      
+      flash.now[:danger] = "There's an error with your login information"
+      
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "See you soon! You are logged out"
+    redirect_to about_path
+    
+  end
+  
+end
