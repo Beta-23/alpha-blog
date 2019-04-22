48f3a3db92e94c59b8dfb7cccfe60dc820cb9b96
Alfred Dominguez <alfred.dominguez@gmail.com>
2019-04-22 20:57:38 +0000

    Add category edit action to admin user

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,20 @@ class CategoriesController < ApplicationController
     end
   end
   
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+      else
+        render 'edit'
+    end
+  end
+  
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
@@ -33,7 +47,7 @@ class CategoriesController < ApplicationController
   
   def require_admin
     if !logged_in? || (logged_in? && !current_user.admin?)
-      flash[:warning] = "Contact Administrator for action!"
+      flash[:warning] = "Contact Administrator for this action!"
       redirect_to categories_path
     end
   end
